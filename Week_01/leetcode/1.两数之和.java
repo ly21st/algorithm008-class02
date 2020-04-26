ab42a2099710a12420a2e8e2551b2b58a6fb6592
ly21st <liyuan21st@foxmail.com>
2020-04-26 23:40:17 +0800

    第二周作业

--- Week_01/leetcode/1.两数之和.java
+++ Week_01/leetcode/1.两数之和.java
@@ -11,24 +11,21 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int []result = new int[2];
         int len = nums.length;
-        Map<Integer, Integer> n = new HashMap<>();
+        int []result = new int[2];
+        if (len < 2) return result;
+        Map<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < len; i++) {
-            if (n.containsKey(target - nums[i])) {
-                result[0] = n.get(target - nums[i]);
+            if (m.containsKey(target - nums[i])) {
+                result[0] = m.get(target - nums[i]);
                 result[1] = i;
-                break;
-            } else {
-                n.put(nums[i], i);
-            }
+                return result;
+            } 
+            m.put(nums[i], i);
         }
         return result;
     }
 }
 // @lc code=end
-
-
-
 
 
